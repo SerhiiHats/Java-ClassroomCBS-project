9f2ca3dc694393425b4c751d7f2fbd2e2e9d736b
Serhi Hats <gatsserv@gmail.com>
2022-05-16 03:17:38 +0300

    Add menu and delete by number

--- Classroom_project1/src/project/controller/Handler.java
+++ Classroom_project1/src/project/controller/Handler.java
@@ -1,57 +1,122 @@
 package project.controller;
-
 import project.dao.BetDao;
 import project.dao.impl.BetDaoImpl;
+import project.db.Storage;
 import project.models.Bet;
-
-import java.lang.reflect.Array;
-import java.util.InputMismatchException;
-import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Handler {
     public static void hand1() {
+        boolean keyout = true;
         Scanner sc = new Scanner(System.in);
-        boolean boolVar = true;
-        int i = 0;
-        int valible1;
-        double risk1;
-        Bet objBet = new Bet();
         BetDao objBDI = new BetDaoImpl();       // objBDI переменная типа интерфейса (с лева обьект типа BetDao, а справа его реализация в BetDaoImpl)
 
-        while (boolVar) {
+        while (keyout) {
+            System.out.println("\u001B[1;32m" + "1.ввести данные 2.показать все данные 3.показать по номеру (№п/п) 4.удалить по номеру (№п/п) 5.(или другой) выход" + "\u001B[0m");
+            switch (sc.next()) {
+                case "1": {
+                    while (true) {
+                        Bet objBet = new Bet();
+                        System.out.println("введите курс : ");
+                        if (sc.hasNextInt()) {
+                            objBet.setValible(sc.nextInt());
+                        } else {
+                            System.out.println("Не коректные данные, пожайлуста повторите");
+                            sc.nextLine();
+                            continue;
+                        }
 
-            try {
-                System.out.println("введите Курс : ");
-                valible1 = sc.nextInt();
-                objBet.setValible(valible1);
-                System.out.println("введите риск : ");
-                risk1 = sc.nextDouble();
-                objBet.setRisk(risk1);
-                System.out.println("Вы ввели курс = " + valible1 + ", и риск = " + risk1);
+                        System.out.println("введите риск : ");
+                        if (sc.hasNextDouble()) {
+                            objBet.setRisk(sc.nextDouble());
+                        } else {
+                            System.out.println("Не коректные данные, пожайлуста повторите");
+                            sc.nextLine();
+                            sc.nextLine();
+                            continue;
+                        }
+                        Storage.bets.add(objBet);          // Добавляем новый элемент в конец списка
+                        System.out.println("\u001B[1;32m" + "Данные записаны. Ваш Курс : " + objBet.getValible() + ", Ваш Риск : " + objBet.getRisk() + "\u001B[0m");
 
-                Bet bet = new Bet(valible1, risk1);
-                objBDI.add(bet);
+                        System.out.println();
+                        System.out.println("Вы хотите продолжить внести другие данные?: " + "\u001B[1;32m" + "Да - любой символ / Нет - \" 0 \":" + "\u001B[0m");
+                        if (sc.next().equals("0")) {
+                            break;
+                        } else {
+                            sc.nextLine();
+                        }
+                    }
+                    break;
+                }
+                case "2": {
+                    if (Storage.bets.size() == 0) {
+                        System.out.println("Список данных пуст");
+                    } else {
+                        for (int i = 0; i < Storage.bets.size(); i++) {
+                            System.out.println("№п/п: " + i + ", " + objBDI.getBet(i));
+                        }
+                    }
+                    break;
+                }
+                case "3": {
+                    while (true) {
+                        try {
+                            System.out.println("Введите номер записи для показа № п/п :");
 
-            } catch (InputMismatchException e) {
-                System.out.println((char) 27 + "[1;31m" + "Вы ввели некоректные данные, проверте еще раз: Курс - только целое число, риск - пишется через \",\" - если число вещественное!" + "\u001B[0m");
-                System.out.println("Введите снова предыдущие данные, но внимательней проверте, поехали!");
-            } catch (NoSuchElementException r) {
+                            if (sc.hasNextInt()) {
+                                int i = sc.nextInt();
+                                System.out.println("№п/п: " + i + ", " + objBDI.getBet(i));
+                                break;
+                            } else {
+                                System.out.println("Номер введен неправильно, пожайлуста повторите");
+                                sc.nextLine();
+                            }
+                        } catch (IndexOutOfBoundsException e) {
+                            System.out.println("\u001B[1;31m" + "Такого номера №п/п: нет" + "\u001B[0m");
+                        } finally {
+                            if (Storage.bets.size() == 0) {
+                                System.out.println("Список данных пуст");
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                }
 
-            }
+                case "4": {
+                    while (true) {
+                        try {
+                            System.out.println("Введите номер записи для удаления № п/п :");
+                            if (sc.hasNextInt()) {
+                                int i = sc.nextInt();
+                                System.out.println("\u001B[1;31m" + "Была удалена запись №п/п: " + i + ", " + objBDI.getBet(i) + " со сдвигом вверх" + "\u001B[0m");
+                                Storage.bets.remove(i);
+                                break;
+                            } else {
+                                System.out.println("Номер введен неправильно, пожайлуста повторите");
+                                sc.nextLine();
+                            }
+                        } catch (IndexOutOfBoundsException e) {
+                            System.out.println("\u001B[1;31m" + "Такого номера №п/п: нет" + "\u001B[0m");
+                        } finally {
+                            if (Storage.bets.size() == 0) {
+                                System.out.println("Список данных пуст");
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                }
 
-            System.out.println("Продолжить ввод данных? введите любой символ - для прекращения введите \"q\"");
-            String vichVvod = sc.next();
-            if (vichVvod.equalsIgnoreCase("q")) {
-                boolVar = false;
+                default: {
+                    System.out.println("Конец программы");
+                    keyout = false;
+                    break;
+                }
             }
         }
-
-        System.out.println(objBDI.get());
-        System.out.println("Конец программы");
         sc.close();
     }
-
 }
 
 
